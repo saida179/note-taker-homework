27f0d28b731b27a4f43a665b81ce50aada3a91b0
saida179 <69127945+saida179@users.noreply.github.com>
2020-10-28 22:17:24 -0700

    forgot to install express

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ Application should allow users to create and save notes.
 Application should allow users to view previously saved notes.
 
 Application should allow users to delete previously saved notes.
+
+
--- sever.js
+++ sever.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const UUID = require("uuid");
-const uel = require("url")
+//const UUID = require("uuid");
+const url = require("url");
+const db = require("./db/db.json")
 
 //express yourself
 const app = express();
@@ -11,6 +12,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+
+module.exports = function(app) {
 
 //GET note
 app.get("/notes", (req, res) => {
@@ -23,7 +27,13 @@ app.get("*", (req, res) => {
 });
 
 //Get /api/notes
-app.get("/api/notees", (req, rest) => {})
+app.get("/api/notes", (req, res) => {
+    fs.readFile('./db/db.json', "utf8", (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        return res.json(notes);
+    });
+})
 //POST /api/notes
 app.post("/api/notes", (req,res) => {})
 //DELETE /api/notes:id
@@ -31,5 +41,7 @@ app.delete("/api/notes/:id", (req, res) => {})
 
 //HEY LISTEN TO THIS
 app.listen(PORT, function () {});
+
+};
 
 //DEPLOY THE APP TO HEROKU...?????
