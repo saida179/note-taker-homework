2fa8696e9e3b2d8da93e63c95b244cc64f4cc100
saida179 <69127945+saida179@users.noreply.github.com>
2021-01-29 01:28:58 -0700

    finished editing apiRoutes

--- routs/apiRoutes.js
+++ routs/apiRoutes.js
@@ -1,2 +1,44 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const UUID = require("uuid")
+const db = require("./db/db.json")
 
+module.exports = function(app) {
 
+    app.get("/api/notes", (req, res) => {
+        fs.readFile('./db/db.json', "utf8", (err, data) => {
+            if (err) throw err;
+            let notes = JSON.parse(data);
+            return res.json(notes);
+        });
+    })
+
+//POST /api/notes
+app.post("/api/notes", (req, res) => {
+    req.body.id = UUID.v1()
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        let newNotes = JSON.parse(data);
+        newNotes.push(req.body);
+
+        fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err) => {
+            if (err) throw err;
+            res.json(req.body);
+        });
+    });
+})
+
+//DELETE /api/notes:id
+app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        let newNotes = JSON.parse(data);
+        newNotes = newNotes.filter(note => note.id !== req.params.id)
+
+        fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err) => {
+            if (err) throw err;
+            res.json("Success!");
+        });
+    });
+});
+
+};
